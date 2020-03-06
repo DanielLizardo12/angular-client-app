ef14b4c29813354da5bd40ffff64a3da8037a172
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2020-03-06 16:12:34 -0600

    welcome page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title: string = 'Welcome to Angular';
+
+  course: string = 'Spring 5 with Angular 7';
+  student: string = 'Daniel Lizardo :D';
 }
